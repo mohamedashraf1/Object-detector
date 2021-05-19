ef7a2490a9b05c747a6ee3bbc5e083cac6245e8c
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-05-20 01:48:24 +0200

    dividing the code into functions

--- Object Detector/object_detector.py
+++ Object Detector/object_detector.py
@@ -5,16 +5,21 @@ import pandas as pd
 import numpy as np
 
 np.set_printoptions(threshold=sys.maxsize)
-
-# read file
-filename = "F:/College/4th year/second term/Computer Vision/Assignments/ass 1/MNIST/mnist_test.csv"
-data_list = pd.DataFrame()
-data_list = pd.read_csv(filename)
-data_list = data_list.drop(data_list.columns[0], axis=1)  # ignore first column and row
+trainingFileName = "F:/College/4th year/second term/Computer Vision/Assignments/ass 1/MNIST/mnist_test.csv"
+testFileName = "F:/College/4th year/second term/Computer Vision/Assignments/ass 1/MNIST/mnist_test.csv"
 featureVectorDataFrame = pd.DataFrame()
 
-# loop for the whole data
-for imageIndex in range(len(data_list.index)):
+
+# read file
+def readFile(name):
+    data_list = pd.DataFrame()
+    data_list = pd.read_csv(name)
+    data_list = data_list.drop(data_list.columns[0], axis=1)  # ignore first column and row
+    return data_list
+
+
+# format the row into 32x32 image
+def getImage(data_list, imageIndex):
     # reading 28x28 image
     imageBefore = np.array(data_list.values[imageIndex], dtype=float).reshape(28, 28)
     # adding 4 more columns right and 4 more rows down to make it 32x32
@@ -28,31 +33,37 @@ for imageIndex in range(len(data_list.index)):
     imageAfter = np.column_stack((imageAfter, zeros2))
     imageAfter = np.column_stack((imageAfter, zeros2))
     imageAfter = np.column_stack((imageAfter, zeros2))
+    return imageAfter
 
-    # calculate magnitude and theta
+
+# calculate magnitude and theta
+def getMagnitudeAndTheta(image):
     magnitude = np.zeros((32, 32), dtype=float)
     theta = np.zeros((32, 32), dtype=float)
     for i in range(32):
         for j in range(32):
             if i == 0:
-                y = imageAfter[i + 1][j]
+                y = image[i + 1][j]
             elif i == 31:
-                y = imageAfter[i - 1][j]
+                y = image[i - 1][j]
             else:
-                y = abs(imageAfter[i - 1][j] - imageAfter[i + 1][j])
+                y = abs(image[i - 1][j] - image[i + 1][j])
             if j == 0:
-                x = imageAfter[i][j + 1]
+                x = image[i][j + 1]
             elif j == 31:
-                x = imageAfter[i][j - 1]
+                x = image[i][j - 1]
             else:
-                x = abs(imageAfter[i][j - 1] - imageAfter[i][j + 1])
+                x = abs(image[i][j - 1] - image[i][j + 1])
             magnitude[i][j] = math.sqrt(x ** 2 + y ** 2)
             if y == 0:
                 theta[i][j] = 0
             else:
                 theta[i][j] = math.degrees(math.atan(x / y)) % 180
+    return magnitude, theta
 
-    # extracting feature vector for each cell
+
+# extracting feature vector for each cell
+def extractCellsFeatureVector(magnitude, theta):
     cellsFeatureVector = np.zeros((4, 4, 1, 9), float)
     for k in range(0, 32, 8):  # image rows
         for m in range(0, 32, 8):  # image columns
@@ -80,20 +91,22 @@ for imageIndex in range(len(data_list.index)):
                         else:
                             featureVector8x8[0][int(angle // 20 + 1)] += magnitude[i][j] - currentPinValue
             cellsFeatureVector[int(k / 8)][int(m / 8)] = featureVector8x8
+    return cellsFeatureVector
 
 
-    def normalize(array, total):
-        if total == 0:
-            return array
-        result = np.zeros((1, 9), float)
-        for i in range(9):
-            result[0][i] = array[0][i] / total
-        return result
+def normalize(array, total):
+    if total == 0:
+        return array
+    result = np.zeros((1, 9), float)
+    for i in range(9):
+        result[0][i] = array[0][i] / total
+    return result
 
 
+# concatenating feature vectors for each 4 cells to make a block feature vector
+def getImageFeatureVector(cellsFeatureVector, imageIndex):
     blocksCounter = 0
     blocksFeatureVector = np.zeros(324, float)  # 9 blocks x 36 values
-    # concatenating feature vectors for each 4 cells to make a block feature vector
     for i in range(0, 3):  # imageFeatureVector rows
         for j in range(0, 3):  # imageFeatureVector columns
             # normalizing the cells before adding them to the block vector
@@ -109,8 +122,23 @@ for imageIndex in range(len(data_list.index)):
             blocksFeatureVector[blocksCounter:blocksCounter + 36, ] = featureVector16x16[:][0]
             blocksCounter += 36
 
-    imageDict = {imageIndex: blocksFeatureVector}
-    featureVectorDataFrame.insert(imageIndex, imageIndex, imageDict[imageIndex])
+    featureVectorDataFrame.insert(imageIndex, imageIndex, blocksFeatureVector)
+
 
 # writing results into an excel file
-featureVectorDataFrame.to_csv("feature-vectors.csv", index=False)
+def writeFeatureVector(DataFrame, fileName):
+    DataFrame.to_csv(fileName, index=False)
+
+
+# apply hog algorithm on a file and writing the results back into an excel file
+def applyHog(fileName, outputFileName):
+    data_list = readFile(fileName)
+    for imageIndex in range(len(data_list.index)):
+        image = getImage(data_list, imageIndex)
+        magnitude, theta = getMagnitudeAndTheta(image)
+        cellsFeatureVector = extractCellsFeatureVector(magnitude, theta)
+        getImageFeatureVector(cellsFeatureVector, imageIndex)
+        writeFeatureVector(featureVectorDataFrame, outputFileName)
+
+
+# applyHog(trainingFileName, "training-output.csv")
